e49c7f80b2de25ee49d2ffcd8ee5b89b857acb61
Jose Perez Huanca <perezhuancajose@gmail.com>
2025-08-22 15:10:15 -0300

    cache en redis

--- routes/ipc.py
+++ routes/ipc.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter,HTTPException
 from playwright.async_api import async_playwright, TimeoutError as PlaywrightTimeoutError
 import logging
+import redis.asyncio as redis
+import json
+import os
+
 
 router=APIRouter(
     prefix="/ipc",
@@ -9,10 +13,24 @@ router=APIRouter(
 
 logging.basicConfig(level=logging.INFO)
 
+redis_client = redis.Redis(
+    host=os.getenv("REDIS_HOST", "localhost"),
+    port=int(os.getenv("REDIS_PORT", 6379)),
+    decode_responses=True  # para que devuelva strings
+)
+
+CACHE_KEY = "ipc_data"
+CACHE_TTL = 43200  # 12 horas
+
 @router.get("/")
 async def get_ipc():
     browser=None
     try:
+        cached_data=await redis_client.get(CACHE_KEY)
+        if cached_data:
+            logging.info("Datos cargados desde redis")
+            return json.loads(cached_data)
+        logging.info("Consultando sitio del INDEC...")
         async with async_playwright() as p:
             browser= await p.chromium.launch(headless=True)
             context= await browser.new_context(
@@ -32,6 +50,8 @@ async def get_ipc():
                 elementos = await page.query_selector_all(selector)
                 textos = [ await el.inner_text() for el in elementos if el]
                 resultados [nombre]= " ".join(t.strip() for t in textos if t.strip())
+            await redis_client.set(CACHE_KEY, json.dumps(resultados), ex=CACHE_TTL)
+            logging.info("Datos almacenados en caché Redis.")
             return resultados
     except PlaywrightTimeoutError:
         raise HTTPException(status_code=504, detail="Tiempo de espera agotado al cargar la página")
